19f32e0154cd371990e57518ba4491ebbd11d8d2
AlohaYos <yos@newtonhawaii.com>
2014-02-02 08:35:47 +0900

    CoreMotion sample code using M7
    CoreMotion sample code. Loggin location and motion activity on map.

---
+++ MotionLog/MotionLogAppDelegate.m
@@ -0,0 +1,38 @@
+//
+//  MotionLogAppDelegate.m
+//  MotionLog
+//
+//  Copyright (c) 2014 Newton Japan. All rights reserved.
+//
+
+#import "MotionLogAppDelegate.h"
+
+@implementation MotionLogAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	[application setIdleTimerDisabled:YES];
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+}
+
+@end
